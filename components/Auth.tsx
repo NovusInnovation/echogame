eee1aa76a6831237a949087257ed6cf833876f7b
Jonathan Bangert <jonathan@bangert.dk>
2024-12-20 14:37:54 -0700

    Add basic login

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -1,75 +1,82 @@
-import React, { useState } from "react";
-import { Alert, StyleSheet, View } from "react-native";
+import { Button, View, StyleSheet } from "react-native";
+import { makeRedirectUri } from "expo-auth-session";
+import * as QueryParams from "expo-auth-session/build/QueryParams";
+import * as WebBrowser from "expo-web-browser";
+import * as Linking from "expo-linking";
 import { supabase } from "../lib/supabase";
-import { Button, Input } from "@rneui/themed";
+import { SetStateAction, useState } from "react";
+
+WebBrowser.maybeCompleteAuthSession(); // required for web only
+const redirectTo = makeRedirectUri();
+
+const createSessionFromUrl = async (url: string) => {
+  const { params, errorCode } = QueryParams.getQueryParams(url);
+
+  if (errorCode) throw new Error(errorCode);
+  const { access_token, refresh_token } = params;
+
+  if (!access_token) return;
+
+  const { data, error } = await supabase.auth.setSession({
+    access_token,
+    refresh_token,
+  });
+  if (error) throw error;
+  return data.session;
+};
+
+const performOAuth = async (setLoading: { (value: SetStateAction<boolean>): void; (arg0: boolean): void; }) => {
+  setLoading(true);
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider: "google",
+    options: {
+      redirectTo,
+      skipBrowserRedirect: true,
+    },
+  });
+  if (error) throw error;
+
+  const res = await WebBrowser.openAuthSessionAsync(
+    data?.url ?? "",
+    redirectTo,
+  );
+
+  if (res.type === "success") {
+    const { url } = res;
+    await createSessionFromUrl(url);
+  }
+
+  setLoading(false);
+};
+
+const anonSignIn = async (setLoading: { (value: SetStateAction<boolean>): void; (arg0: boolean): void; }) => {
+  setLoading(true);
+  const { error } = await supabase.auth.signInAnonymously();
+
+  if (error) throw error;
+  setLoading(false);
+};
 
 export default function Auth() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  // Handle linking into app from email app.  
   const [loading, setLoading] = useState(false);
-
-  async function signInWithEmail() {
-    setLoading(true);
-    const { error } = await supabase.auth.signInWithPassword({
-      email: email,
-      password: password,
-    });
-
-    if (error) Alert.alert(error.message);
-    setLoading(false);
-  }
-
-  async function signUpWithEmail() {
-    setLoading(true);
-    const {
-      data: { session },
-      error,
-    } = await supabase.auth.signUp({
-      email: email,
-      password: password,
-    });
-
-    if (error) Alert.alert(error.message);
-    if (!session)
-      Alert.alert("Please check your inbox for email verification!");
-    setLoading(false);
-  }
+  const url = Linking.useURL();
+  if (url) createSessionFromUrl(url);
 
   return (
     <View style={styles.container}>
       <View style={[styles.verticallySpaced, styles.mt20]}>
-        <Input
-          label="Email"
-          leftIcon={{ type: "font-awesome", name: "envelope" }}
-          onChangeText={(text) => setEmail(text)}
-          value={email}
-          placeholder="email@address.com"
-          autoCapitalize={"none"}
-        />
-      </View>
-      <View style={styles.verticallySpaced}>
-        <Input
-          label="Password"
-          leftIcon={{ type: "font-awesome", name: "lock" }}
-          onChangeText={(text) => setPassword(text)}
-          value={password}
-          secureTextEntry={true}
-          placeholder="Password"
-          autoCapitalize={"none"}
-        />
-      </View>
-      <View style={[styles.verticallySpaced, styles.mt20]}>
         <Button
-          title="Sign in"
+          title="Sign in with Google"
           disabled={loading}
-          onPress={() => signInWithEmail()}
+          onPress={() => performOAuth(setLoading)}
         />
       </View>
       <View style={styles.verticallySpaced}>
         <Button
-          title="Sign up"
+          title="Sign in anon"
           disabled={loading}
-          onPress={() => signUpWithEmail()}
+          onPress={() => anonSignIn(setLoading)}
         />
       </View>
     </View>
